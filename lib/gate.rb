ad5e8fc35bf46e7927635e81b6fbe039d558315f
佐藤　祐弥 <y.satou@satouyuuwatarunoMacBook-Air.local>
2020-09-16 15:53:53 +0900

    finish gate_program

--- lib/gate.rb
+++ lib/gate.rb
@@ -1,14 +1,25 @@
 class Gate
+    STATIONS = [:umeda, :juso, :mikuni]
+    FARES = [150, 190]
+
     def initialize(name)
       @name = name  
     end 
+
+    def calc_fare(ticket)
+        from = STATIONS.index(ticket.stamped_at)
+        to = STATIONS.index(@name)
+        distance = to - from
+        FARES[distance - 1]
+    end
 
     def enter(ticket)
         ticket.stamp(@name)
     end
 
     def exit(ticket)
-        true
+        fare = calc_fare(ticket)
+        fare <= ticket.fare 
     end
 
 end
